ef08de6746a5dd68bbac9501710049ca4ccb34bc
foxlovecat1989 <foxlovecat1989@gmail.com>
2021-11-22 18:50:41 +0800

    refactor code

--- src/app/model/Stock.ts
+++ src/app/model/Stock.ts
@@ -9,6 +9,22 @@ export class Stock{
   changePrice!: number;
   changeInPercent!: number;
   volume!: number;
-  transactionDate!: string;
+  lastUpDateTime!: string;
   classify!: Classify;
+
+  static fromHttp(data: Stock): Stock {
+    const stock = new Stock();
+    stock.id = data.id;
+    stock.name = data.name;
+    stock.classify = data.classify;
+    stock.symbol = data.symbol;
+    stock.changeInPercent = data.changeInPercent;
+    stock.changePrice = data.changePrice;
+    stock.previousClosed = data.previousClosed;
+    stock.price = data.price;
+    stock.volume = data.volume;
+    stock.lastUpDateTime = data.lastUpDateTime;
+
+    return stock;
+  }
 }
